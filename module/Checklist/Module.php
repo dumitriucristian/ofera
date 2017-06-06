f9d425556b3c6132e3fd78fa40f15c1a9c15e431
webRookie <dumitriucristian@yahoo.com>
2017-06-06 17:25:23 +0300

    Config

--- module/Checklist/Module.php
+++ module/Checklist/Module.php
@@ -7,7 +7,7 @@
  * @license   http://framework.zend.com/license/new-bsd New BSD License
  */
 
-namespace Module;
+namespace Checklist;
 
 use Checklist\Model\TaskMapper;
 use Zend\Mvc\ModuleRouteListener;
